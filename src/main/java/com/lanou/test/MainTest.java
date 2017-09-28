e59566c55658adf0dc97e8933ce2ecddf9af3857
dllo <13252984305@163.com>
2017-09-28 15:18:09 +0800

    第二次提交,修改添加一行输出语句,在MainTest文件中

--- src/main/java/com/lanou/test/MainTest.java
+++ src/main/java/com/lanou/test/MainTest.java
@@ -6,6 +6,8 @@ package com.lanou.test;
 public class MainTest {
 
     public static void main(String[] args) {
+
+        System.out.println("你好");
         System.out.println("别睡觉,会死!");
     }
 }
